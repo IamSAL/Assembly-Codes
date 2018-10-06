c74cca4d05c6839cb9357f2f321ed72c91dede5e
Sheikh Salman <sheikhsalman2013@gmail.com>
2018-10-06 11:14:03 +0600

    STRING ASM

---
+++ STRING PRINT.asm
@@ -0,0 +1,20 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+MSG1 DB "HELLO World. $"
+
+.CODE
+
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    LEA DX,MSG1
+    MOV AH,9
+    INT 21H
+    
+    MOV AH,4CH
+    INT 21H
+    MAIN ENDP
+
+END MAIN
